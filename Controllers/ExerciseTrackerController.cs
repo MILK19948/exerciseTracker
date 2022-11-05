40e90761ff12c147dacbe60cbf38ff54abe0a816
Amilcar Cornier <cornier25@yahoo.com>
2022-11-04 23:55:01 -0400

    Exercise Tracker

---
+++ Controllers/ExerciseTrackerController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using exerciseTracker.Models;
+using exerciseTrackerDataAccess.Context;
+using exerciseTrackerDataAccess.Moldels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace exerciseTracker.Controllers
+{
+    public class ExerciseTrackerController : Controller
+    {
+        private readonly ExerciseTrackerDbContext _context;
+
+        public ExerciseTrackerController(ExerciseTrackerDbContext context)
+        {
+            _context = context;
+        }
+        // GEt
+        public IActionResult Index()
+        {
+            ExerciseTrackerViewModel model = new ExerciseTrackerViewModel(_context);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(long exerciseId, string FirstName, string SecondName, string WorkoutName, string WorkoutTime)
+        {
+            ExerciseTrackerViewModel model = new ExerciseTrackerViewModel(_context);
+
+            ExerciseTrackerModel exercises = new(exerciseId, FirstName, SecondName, WorkoutName, WorkoutTime);
+
+            model.SaveExercise(exercises);
+            model.IsActionSuccess = true;
+            model.ActionMessage = "Exercise has been saved successfully";
+
+            return View(model);
+        }
+
+        public IActionResult Update(long id)
+        {
+            ExerciseTrackerViewModel model = new(_context, id);
+
+            return View(model);
+        }
+
+        public IActionResult Delete(long id)
+        {
+           ExerciseTrackerViewModel model = new(_context);
+
+            if (id > 0)
+            {
+                model.RemoveExercise(id);
+                model.IsActionSuccess = true;
+                model.ActionMessage = "Exercise has been deleted successfully";
+            }
+
+            return View("Index", model);
+        }
+    }
+}
---
+++ Models/ExerciseTrackerViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using exerciseTrackerDataAccess.Context;
+using exerciseTrackerDataAccess.Moldels;
+using exerciseTrackerDataAccess.Repositories;
+
+namespace exerciseTracker.Models
+{
+    public class ExerciseTrackerViewModel
+    {
+        private readonly ExerciseTrackerRepository _repo;
+        public List<ExerciseTrackerModel> ExerciseList { get; set; }
+        public ExerciseTrackerModel CurrentExercise { get; set; }
+
+        public bool IsActionSuccess { get; set; }
+
+        public string ActionMessage { get; set; } = "";
+
+        public ExerciseTrackerViewModel(ExerciseTrackerDbContext context)
+        {
+            _repo = new ExerciseTrackerRepository(context);
+            ExerciseList = GetAllExercise();
+            CurrentExercise = ExerciseList.FirstOrDefault()!;
+        }
+
+        public ExerciseTrackerViewModel(ExerciseTrackerDbContext context, long exerciseId)
+        {
+            _repo = new ExerciseTrackerRepository(context);
+            ExerciseList = GetAllExercise();
+            CurrentExercise = exerciseId > 0 ? GetExercise(exerciseId) : new ExerciseTrackerModel();
+        }
+
+        public void SaveExercise(ExerciseTrackerModel exercise)
+        {
+            if (exercise.ID > 0)
+            {
+                _repo.Update(exercise);
+            }
+            else
+            {
+                exercise.ID = _repo.Create(exercise);
+            }
+
+            ExerciseList = GetAllExercise();
+            CurrentExercise = GetExercise(exercise.ID);
+        }
+
+        public void RemoveExercise(long exerciseId)
+        {
+            _repo.Delete(exerciseId);
+            ExerciseList = GetAllExercise();
+            CurrentExercise = ExerciseList.FirstOrDefault()!;
+        }
+
+        public List<ExerciseTrackerModel> GetAllExercise()
+        {
+            return _repo.GetAllExercises();
+        }
+
+        public ExerciseTrackerModel GetExercise(long exerciseId)
+        {
+            return _repo.GetExerciseById(exerciseId);
+        }
+    
+    }
+}
+
